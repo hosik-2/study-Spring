a81d73270b9c09c421aa7278123ff6a03844736b
hosik-2 <zxvbcd123@gmail.com>
2026-07-17 01:58:47 +0900

    트랜잭션 및 DB락 학습 완료

---
+++ spring-db1-study/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV1.java
@@ -0,0 +1,143 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.connection.DBConnectionUtil;
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.support.JdbcUtils;
+import org.springframework.lang.NonNull;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.NoSuchElementException;
+
+/**
+ * JDBC - DataSource 사용, JdbcUtils 사용
+ */
+
+@Slf4j
+public class MemberRepositoryV1 {
+
+    private final DataSource dataSource; // 생성자로 의존성 주입 받기
+
+    public MemberRepositoryV1(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public Member save(Member member) throws SQLException { // try-catch에서 나온 예외 던지기!
+        String sql = "insert into member(member_id, money) values (?, ?)";
+
+        Connection con = null;
+        PreparedStatement pstmt = null; // 이걸 가지고 DB에 쿼리를 날릴 수 있음
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql); // 커넥션에 연결된 정보를 바탕으로 statement세팅
+            /**
+             * 앞으로 커밋, 수정, 롤백 등 모든 것은 커넥션으로 한다고 함. DB와 연결된 통로이기 때문(연결 정보가 있음)
+             * 그리고 pstmt는 statement의 종류로 미리 준비해놓는(일반 statement는 보안 및 효율 안좋음) 것, 파라미터를 바인딩 할 수 있음
+             * 즉, 저렇게 쿼리를 먼저 세팅하고 대기시켜놓은 다음 ?에 해당하는 벨류만 나중에 수정해서 올릴 수 있당
+             */
+            pstmt.setString(1, member.getMemberId()); // ?에다가 넣는 겁니다
+            pstmt.setInt(2, member.getMoney());
+            pstmt.executeUpdate(); // 준비해둔 설정을 반영해서 SQL 실행!
+            return member; // 객체 반환 -> member(member_id, money)
+        } catch (SQLException e) {
+            log.info("db error", e);
+            throw e; // 발생하는 예외를 여기서 처리하는 것이 아닌 밖으로 던지기
+        } finally { // 예외가 나든 말든 커넥션과 pstmt 자원 닫기
+            close(con, pstmt, null);
+        }
+
+    }
+
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId); // 파라미터로 받은 memberId값을 넘겨서 쿼리 세팅
+
+            rs = pstmt.executeQuery();// Query는 select, Update는 데이터를 변경할 때 쓰는 것!
+            if (rs.next()) { //cursor가 처음엔 데이터가 없는 첫 빈공간에 가있음 이걸 한 번 넘겨주는 것임
+                //rs.next() -> 다음으로 넘겼을 때 boolean 값을 반환함
+                Member member = new Member(); // DB응답을 받을 객체 생성
+                member.setMemberId(rs.getString("member_id")); //ResultSet에 저장된 DB응답 결과 빼오기
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found memberId=" + memberId);
+            }
+
+        } catch (SQLException e) {
+            log.info("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, rs);
+        }
+
+    }
+
+    public void update(String memberId, int money) throws SQLException {
+        String sql = "update member set money=? where member_id=?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, money);
+            pstmt.setString(2, memberId);
+            int resultSize = pstmt.executeUpdate(); // executeUpdate()는 영향을 받은 행(row)의 갯수를 반환해줌(int)
+            log.info("resultSize={}", resultSize);
+
+        } catch (SQLException e) {
+            log.info("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, null);
+        }
+
+    }
+
+    public void delete(String memberId) throws SQLException {
+        String sql = "delete from member where member_id=?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            pstmt.executeUpdate(); // executeUpdate()는 영향을 받은 행(row)의 갯수를 반환해줌(int)
+
+        } catch (SQLException e) {
+            log.info("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, null);
+        }
+    }
+
+    private void close(Connection con, Statement stmt, ResultSet rs) {
+        JdbcUtils.closeResultSet(rs);
+        JdbcUtils.closeStatement(stmt);
+        JdbcUtils.closeConnection(con);
+    }
+
+    @NonNull
+    private Connection getConnection() throws SQLException {
+        Connection con = dataSource.getConnection();
+        log.info("get connection={}, class={}", con, con.getClass());
+        return con;
+
+    }
+
+
+}
---
+++ spring-db1-study/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
@@ -0,0 +1,72 @@
+package hello.jdbc.repository;
+
+
+import com.zaxxer.hikari.HikariDataSource;
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Slf4j
+class MemberRepositoryV1Test {
+
+    MemberRepositoryV1 repository;
+
+    @BeforeEach
+    void beforeEach() {
+        // 기본 DriverManager - 항상 새로운 커넥션을 획득
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD); // dataSource 세팅
+
+        //커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+
+        repository = new MemberRepositoryV1(dataSource); // V1 생성자 파라미터 dataSource 주입
+
+    }
+
+    @Test
+    void crud() throws SQLException {
+        //save
+        Member member = new Member("memberV3", 10000);
+        repository.save(member);
+
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        assertThat(findMember).isEqualTo(member);
+
+        //update - money: 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember).isEqualTo(new Member("memberV3", 20000));
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        //delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
+        // 쉽게 얘기해서 ThrownBy니까 던졌다 무언가를 <- 이뜻으로 이해하고 파라미터 안에는 예외가 "던져질" 로직을 적고
+        // 그 예외가 무슨 예외냐?를 isInstanceOf()에 인자를 예외 클래스 정보를 써주면 된다!
+        // 그리고 왜 NoSuchElementException 이게 나오냐면 우리가 findById 메서드에 rs.netx()가 false가 나올 시
+        // else 문에다가 NoSuchElementException을 던지게 만들었기 때문임
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
